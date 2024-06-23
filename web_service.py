d9b526ded85751fb05de2c2b7dfb0829c579dd0d
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2024-06-23 15:44:49 -0300

    project config

--- web_service.py
+++ web_service.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+import uvicorn
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI(
+    title="hello-bruno-api",
+    version="0.1.0",
+    description="test render",
+)
 
 
-@app.route('/', methods=['GET'])
-def receive_event():
-    return jsonify({"status": "success", "message": "Hello Bruno API"}), 200
+@app.get('/')
+async def root():
+    return {"message": "Hello Bruno!"}
 
 
-if __name__ == '__main__':
-    # Run the app on the local network, accessible on port 5000
-    app.run(host='0.0.0.0', port=2999)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
