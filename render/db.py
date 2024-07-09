0d433bf550a5d3f9d12cccc92b338b3b261100ef
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2024-07-09 12:21:16 -0300

    dependency injection for collection

--- render/db.py
+++ render/db.py
@@ -1,15 +1,20 @@
 import motor.motor_asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from render.config import settings
 from fastapi import Depends
 
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_uri)
 db = client.get_default_database(settings.mongodb_db)
-actins_collection = db.get_collection("actions")
+users_collection = db.get_collection("users")
 
 
 async def get_database() -> AsyncIOMotorClient:
     return db.client
 
-
 ActiveSession = Depends(get_database)
+
+
+async def get_user_collection():
+    return users_collection
+
+ActiveSession = Depends(get_user_collection)
